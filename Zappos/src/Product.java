6c8cd06cc20d12cc1760dcb573390039cdb91341
fordfiler6 <ffiler@usc.edu>
2014-09-26 15:29:31 -0700

    Added url storage to support better output, also fixed linking the json jar

--- Zappos/src/Product.java
+++ Zappos/src/Product.java
@@ -3,10 +3,14 @@ public class Product
 {
 	String productId;
 	double price;
-	public Product(String productId, double price)
+	String productPageUrl;
+	String productImageUrl;
+	public Product(String productId, double price, String productPageUrl, String productImageUrl)
 	{
 		this.productId = productId;
 		this.price = price;
+		this.productPageUrl = productPageUrl;
+		this.productImageUrl = productImageUrl;
 	}
 
 }
